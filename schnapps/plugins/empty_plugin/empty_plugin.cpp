f35533ac9ffbb37c1e3ce46e8ff431a66024f612
Étienne Schmitt <etienne.schmitt@inria.fr>
2016-07-27 11:46:28 +0200

    added "Empty Plugin".
    Signed-off-by: Étienne Schmitt <etienne.schmitt@inria.fr>

---
+++ schnapps/plugins/empty_plugin/empty_plugin.cpp
@@ -0,0 +1,49 @@
+/*******************************************************************************
+* SCHNApps                                                                     *
+* Copyright (C) 2016, IGG Group, ICube, University of Strasbourg, France       *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Web site: http://cgogn.unistra.fr/                                           *
+* Contact information: cgogn@unistra.fr                                        *
+*                                                                              *
+*******************************************************************************/
+
+#include <empty_plugin.h>
+#include <schnapps/core/schnapps.h>
+
+namespace schnapps
+{
+
+namespace plugin_empty_plugin
+{
+
+EmptyPlugin::EmptyPlugin()
+{}
+
+EmptyPlugin::~EmptyPlugin()
+{}
+
+bool EmptyPlugin::enable()
+{
+	return true;
+}
+
+void EmptyPlugin::disable()
+{
+}
+
+} // namespace plugin_empty_plugin
+} // namespace schnapps
